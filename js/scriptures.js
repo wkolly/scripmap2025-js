33e63acb38ee514f756b748f1a69d1ad5ae30c65
Steve Liddle <liddle@byu.edu>
2025-02-07 14:06:11 -0700

    Finished recording video 10, refactoring

--- js/scriptures.js
+++ js/scriptures.js
@@ -7,112 +7,6 @@
  *              IS 542, Winter 2025, BYU.
  */
 
-const Html = (function () {
-    ("use strict");
-
-    /*------------------------------------------------------------------
-     *                      PUBLIC CONSTANTS
-     */
-    const TAG_DIV = "div";
-    const TAG_HEADER5 = "h5";
-    const TAG_HYPERLINK = "a";
-    const TAG_I = "i";
-    const TAG_LIST_ITEM = "li";
-    const TAG_UNORDERED_LIST = "ul";
-
-    /*------------------------------------------------------------------
-     *                      PUBLIC METHOD DECLARATIONS
-     */
-    let decodeEntities;
-    let decorateNode;
-    let domNode;
-    let hyperlinkNode;
-    let replaceNodeContent;
-
-    /*------------------------------------------------------------------
-     *                      PUBLIC METHODS
-     */
-    decodeEntities = function (text) {
-        // Replace all &mdash; entities with the actual mdash character.
-        // If we have content in the future with other HTML entities, we
-        // would need to update this method.  If this gets a bit bigger,
-        // I would build a dictionary with the key/value pairs desired
-        // and then iterate over it, e.g. {"&mdash;": "—", "&amp;": "&"}
-
-        return text.replace("&mdash;", "—");
-    };
-
-    decorateNode = function (node, clazz, id) {
-        // Add class and/or id attributes as requested
-
-        if (clazz) {
-            for (const word of clazz.split(" ")) {
-                node.classList.add(word);
-            }
-        }
-
-        if (id) {
-            node.id = id;
-        }
-    };
-
-    domNode = function (tag, clazz, id, textContent) {
-        // Build a DOM node for the given tag type.
-        // Note that because "class" is a reserved word, it's conventional
-        // to use the word "clazz" as an identifier in C-like languages.
-
-        const node = document.createElement(tag);
-
-        decorateNode(node, clazz, id);
-
-        if (textContent) {
-            node.appendChild(document.createTextNode(textContent));
-        }
-
-        return node;
-    };
-
-    hyperlinkNode = function (href, title, clazz, id) {
-        // Build a hyperlink node with the given attributes
-
-        const hyperlink = document.createElement(TAG_HYPERLINK);
-
-        if (href) {
-            hyperlink.href = href;
-        }
-
-        if (title) {
-            hyperlink.title = title;
-        }
-
-        decorateNode(hyperlink, clazz, id);
-
-        return hyperlink;
-    };
-
-    replaceNodeContent = function (node, newChild) {
-        while (node.firstChild) {
-            node.removeChild(node.firstChild);
-        }
-
-        node.appendChild(newChild);
-    };
-
-    return {
-        decodeEntities,
-        decorateNode,
-        domNode,
-        hyperlinkNode,
-        replaceNodeContent,
-        TAG_DIV,
-        TAG_HEADER5,
-        TAG_HYPERLINK,
-        TAG_I,
-        TAG_LIST_ITEM,
-        TAG_UNORDERED_LIST
-    };
-})();
-
 const Scriptures = (function () {
     "use strict";
 
@@ -123,7 +17,9 @@ const Scriptures = (function () {
     const CLASS_BUTTON = "waves-effect waves-custom waves-ripple btn";
     const CLASS_CHAPTER = "chapter";
     const CLASS_VOLUME = "volume";
-    const ID_NAV_ELEMENT = "scrip-nav";
+    const ID_CRUMBS = "crumbs";
+    const ID_CRUMBS_COMPLEMENT = "crumbs-complement";
+    const ID_NAV_ELEMENT = "nav-root";
     const ID_SCRIPTURES_NAVIGATION = "scripnav";
     const SKIP_JSON_PARSE = true;
     const URL_BASE = "https://scriptures.byu.edu/mapscrip/";
@@ -135,6 +31,9 @@ const Scriptures = (function () {
      *                      PRIVATE VARIABLES
      */
     let books;
+    let crumbsElement;
+    let crumbsComplementElement;
+    let navElement;
     let volumes;
 
     /*------------------------------------------------------------------
@@ -303,8 +202,6 @@ const Scriptures = (function () {
     };
 
     getScripturesFailure = function () {
-        const navElement = document.getElementById("scrip-nav");
-
         Html.replaceNodeContent(
             navElement,
             document.createTextNode("Unable to retrieve chapter contents.")
@@ -312,8 +209,6 @@ const Scriptures = (function () {
     };
 
     getScripturesSuccess = function (chapterHtml) {
-        const navElement = document.getElementById("scrip-nav");
-
         navElement.innerHTML = chapterHtml;
         // NEEDSWORK: update breadcrumbs
         // NEEDSWORK: update pins on the map
@@ -328,7 +223,6 @@ const Scriptures = (function () {
     };
 
     navigateBook = function (bookId) {
-        const navElement = document.getElementById("scrip-nav");
         const book = books[bookId];
 
         if (book.numChapters <= 1) {
@@ -356,8 +250,6 @@ const Scriptures = (function () {
     };
 
     navigateHome = function (volumeId) {
-        let navElement = document.getElementById("scrip-nav");
-
         const scripturesNavigationNode = Html.domNode(Html.TAG_DIV, null, ID_SCRIPTURES_NAVIGATION);
 
         buildVolumesGrid(scripturesNavigationNode, volumeId);
@@ -384,44 +276,32 @@ const Scriptures = (function () {
     /*------------------------------------------------------------------
      *                      PUBLIC METHODS
      */
-    init = function () {
+    init = function (callback) {
         let booksIsLoaded = false;
         let volumesIsLoaded = false;
-
-        function displayVolumes() {
-            const scripNav = document.getElementById(ID_NAV_ELEMENT);
-
-            volumes.forEach((volume) => {
-                const element = document.createElement("li");
-
-                element.textContent = volume.fullName;
-                scripNav.appendChild(element);
-
-                volume.books.forEach((book) => {
-                    const element = document.createElement("li");
-
-                    element.textContent = book.fullName;
-                    scripNav.appendChild(element);
-                });
-            });
-        }
 
         ajax(URL_BOOKS, (json) => {
             books = json;
             booksIsLoaded = true;
 
             if (volumesIsLoaded) {
-                cacheBooks(displayVolumes);
+                cacheBooks(callback);
             }
         });
+
         ajax(URL_VOLUMES, (json) => {
             volumes = json;
             volumesIsLoaded = true;
 
             if (booksIsLoaded) {
-                cacheBooks(displayVolumes);
+                cacheBooks(callback);
             }
         });
+
+        // look up all the DOM elements we want to manipulate
+        crumbsElement = document.getElementById(ID_CRUMBS);
+        crumbsComplementElement = document.getElementById(ID_CRUMBS_COMPLEMENT);
+        navElement = document.getElementById(ID_NAV_ELEMENT);
     };
 
     onHashChanged = function () {
